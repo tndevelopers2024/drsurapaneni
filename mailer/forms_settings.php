284222a916ba99913ba6e67c0ba88367a92b48d0
tndevelopers2024 <168084182+tndevelopers2024@users.noreply.github.com>
2024-12-02 17:19:41 +0530

    Add files via upload

---
+++ mailer/forms_settings.php
@@ -0,0 +1,8 @@
+<?php
+	/* Forms settings */
+	$subj = "New message from the site 'TRETO - Resume Template'"; //letter subject
+	$to = 'yourname@domain.com'; // Enter Your E-mail
+	$from = 'admin@domain.com'; // Admin e-mail
+	$fromName = 'TRETO'; // Your company name
+	$charset = 'UTF-8';
+?>
